655e00c7799aee08b69a00db57c0801174c231c9
Vanessa Cavaco Branco <vanessa.cavaco.branco@gmail.com>
2023-06-03 16:28:32 +0200

    Exercicío Binary Trees

--- src/binary_tree.js
+++ src/binary_tree.js
@@ -26,9 +26,48 @@ class BinaryTree {
 
     function traverse(node) {
       // Tu código aquí
+
+      //primero he conseguido hacer con Pila
+      /*
+      let stack = [];
+      //hago push del valor de la root al array result y adiciono a stack
+      stack.push(node);
+
+      //itero por el arbole para recoger derecha y izquierda y adicinar sus valores al array result
+      while (stack.length > 0) {
+        //saco el el primer elemento de la fila 
+        const currentNode = stack.pop();
+        console.log(currentNode)
+        //y adiciono su valor al array de results
+        result.push(currentNode.value);
+        //ahora pasmos a los nodos derecha y izquierda
+        //verifico si el left del nodo actual es diferent de null, y si es diferente de null adiciono su nodo a la stack
+      
+        if (currentNode.right !== null) {
+          stack.push(currentNode.right);
+        }
+        if (currentNode.left !== null) {
+          stack.push(currentNode.left);
+        }
+      }
+      */
+
+      //después probé hacer con recursividad
+      if (node == null) return;
+      //adiciona el valor del nodo actual al array results
+      result.push(node.value);
+      //recursivamente llama traverse por el nodo izquierdo
+      if (node.left !== null) {
+        traverse(node.left);
+      }
+      //recursivamente llama traverse por el nodo derecho
+      if (node.right !== null) {
+        traverse(node.right);
+      }
     }
 
     traverse(this);
+    //console.log(result);
     return result;
   }
 }
--- test/binary_tree.test.js
+++ test/binary_tree.test.js
@@ -26,5 +26,17 @@ describe("Binary Tree", () => {
       binaryTree.add(12);
       expect(binaryTree.getValues()).toEqual([10, 4, 2, 6, 14, 12, 16]);
     });
+    test("Should return an array with lenght equal 7", () => {
+      binaryTree.add(4);
+      binaryTree.add(14);
+      binaryTree.add(6);
+      binaryTree.add(16);
+      binaryTree.add(2);
+      binaryTree.add(12);
+      expect(binaryTree.getValues().length).toEqual(7);
+    });
+    test("Should return true if method getValues exist", () => {
+      expect(typeof binaryTree.getValues).toBe(`function`);
+    });
   });
 });
